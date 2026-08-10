b539c4982fcc2a693b089def8f12e3336f0ff16f
EvolutionLogicChain <EvolutionLogicChain@users.noreply.github.com>
2026-08-10 23:45:51 +0000

    deploy: 376f9849aa89b7deec6ca26f29413a3ac47e9b66

---
+++ extra.js
@@ -0,0 +1,10 @@
+﻿(function() {
+  var path = window.location.pathname;
+  if (path.indexOf('_cn') !== -1) return;
+  var ref = document.referrer;
+  if (ref && ref.indexOf(window.location.hostname) !== -1) return;
+  var lang = navigator.language || navigator.userLanguage || '';
+  if (lang.startsWith('zh') && path.endsWith('/')) {
+    window.location.href = path + 'README_cn/';
+  }
+})();
