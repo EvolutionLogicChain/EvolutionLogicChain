82f63c684f3b8cfbe45e10dee01dccf14b36c910
FreeClawAI <jiaoyiweisheng@foxmail.com>
2026-08-11 07:36:44 +0800

    Add foreword, i18n redirect, navigation breadcrumbs

--- extra.js
+++ extra.js
@@ -1,9 +1,10 @@
 ﻿(function() {
-  var lang = navigator.language || navigator.userLanguage || '';
-  if (!lang.startsWith('zh')) return;
   var path = window.location.pathname;
   if (path.indexOf('_cn') !== -1) return;
-  if (path.endsWith('/')) {
+  var ref = document.referrer;
+  if (ref && ref.indexOf(window.location.hostname) !== -1) return;
+  var lang = navigator.language || navigator.userLanguage || '';
+  if (lang.startsWith('zh') && path.endsWith('/')) {
     window.location.href = path + 'README_cn/';
   }
 })();
